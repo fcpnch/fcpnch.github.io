aa26175a25e59932cf6174e51879171c4dc29aac
Lennart Bernhardt <l.bernhardt@live.de>
2016-06-01 18:19:17 +0200

    fix userscript

--- userscript/fcpnch.user.js
+++ userscript/fcpnch.user.js
@@ -2,11 +2,15 @@
 // @name          fcpn.ch
 // @author        Lenny. (@ohitsLenny)
 // @namespace     fcpn.ch
+// @version       1.1
 // @description   This will add a buton to shorten and copy links
 // @include       *facepunch.com/showthread*
 // @require       jquery.js
 // @resource      fpnchcss fcpnch.css
-// @version       1.0
+// @grant         GM_setClipboard
+// @grant         GM_getResourceText
+// @grant         GM_addStyle
+// @run-at document-idle
 // ==/UserScript==
 
 var myCss = GM_getResourceText("fpnchcss");
@@ -14,27 +18,27 @@ GM_addStyle(myCss);
 
 var lookUp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 function base62(n, b) {
-	var res = []
-	do {
-		var idk = n % b;
-		res.unshift(lookUp.charAt(idk))
-		n = Math.floor(n / b)
-	} while (n > 0);
-	return res.join("");
+    var res = [];
+    do {
+        var idk = n % b;
+        res.unshift(lookUp.charAt(idk));
+        n = Math.floor(n / b);
+    } while (n > 0);
+    return res.join("");
 }
 
 var shortner = document.createElement("a");
 shortner.innerHTML = "shortn";
-shortner.className = "fcpnchBtn"
+shortner.className = "fcpnchBtn";
 
 $(".nodecontrols").prepend(shortner);
 
 $(".fcpnchBtn").click(function(event) {
-	var postID = $(this).next()[0].name;
-	postID = Number(postID.replace("post", ""));
+    var postID = $(this).next()[0].name;
+    postID = Number(postID.replace("post", ""));
 
-	var shortPostID = base62(postID, 62);
+    var shortPostID = base62(postID, 62);
 
-	var toCopy = "http://fcpn.ch#" + shortPostID;
-	GM_setClipboard(toCopy);
+    var toCopy = "http://fcpn.ch/" + shortPostID;
+    GM_setClipboard(toCopy);
 });
